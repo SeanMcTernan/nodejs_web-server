e8471cf8908446bab10fd22b6ec961c0b4620876
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-10-29 17:53:36 -0600

    Add high and low temperatures to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,7 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/fefa427dc1d22f485167ddcb224462b4/' + lat + ',' + long + '?units=si'
+    console.log(url)
     
     request({ url, json: true }, (error, { body }) => {
         
@@ -14,12 +15,13 @@ const forecast = (lat, long, callback) => {
         else {
             const temp = body.currently.temperature
             const percip = body.currently.precipProbability
-            callback(undefined, body.daily.data[0].summary + ` It is currenty ${temp} out. There is a ${percip} percent chance of rain.`)
+            const high = body.daily.data[0].temperatureHigh
+            const low = body.daily.data[0].temperatureLow
+            callback(undefined, body.daily.data[0].summary + ` It is currenty ${temp} out. There is a ${percip} percent chance of rain. Today there is a high of ${high} and low of ${low}.`)
         }
     })
 
 }
 
 module.exports = forecast
-
 
